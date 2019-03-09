3a5dea364124838a2ea4277e034dbef254f2c716
Liv Cella <livncella@gmail.com>
2019-03-09 11:27:51 -0700

    seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'comments/show'
-  get 'comments/new'
-  get 'comments/edit'
+
+  root 'user#index'
   devise_for :users
+  resources :accounts
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
