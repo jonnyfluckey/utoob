264948f13ed93d4364024388e6e2b6bc72e76b85
Liv Cella <livncella@gmail.com>
2019-03-09 12:18:05 -0700

    added helper method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,13 +16,13 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
 
     if @movie.save
-      
+
       redirect_to movies_path
     else
       render :new
     end
   end
-  
+
   def edit
   end
 
