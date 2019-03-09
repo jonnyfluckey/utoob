4ad0fcb120af06689500cf86826dfb35dd82d856
Marisoln Araujo <u167272@gmail.com>
2019-03-09 11:38:39 -0700

    updated comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,27 @@
 class CommentsController < ApplicationController
+
+  before_action :set_comment , only: [:show, :edit, :new, :destroy]
+
   def index
+    @comments = Comment.
+    @comments = Comment.new
   end
 
   def show
   end
 
   def new
+    @comments = Comment.new
   end
 
   def edit
   end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+
 end
