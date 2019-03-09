7a1eac4515d75fa83fc02b832f810f94108338d2
Marisoln Araujo <u167272@gmail.com>
2019-03-09 11:00:48 -0700

    added seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_173148) do
+ActiveRecord::Schema.define(version: 2019_03_09_173326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_03_09_173148) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "movies"
 end
