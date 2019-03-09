1e38c5e9deefd21036015cbc240ece8656534514
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-03-09 10:46:55 -0700

    added comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/show'
+  get 'comments/new'
+  get 'comments/edit'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
